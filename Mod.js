94ae69ebe6c672e171544452c4503cb0f7fead2b
Pookstir <134732433+Pookstir@users.noreply.github.com>
2023-05-28 11:23:46 -0700

    Update Mod.js

--- Mod.js
+++ Mod.js
@@ -128,6 +128,7 @@ func:function()
 			{type:'gather',context:'fungal farm',what:{'stick':0.5},amount:1,max:3},
 			{type:'gather',context:'fungal farm',what:{'spore':0.25},amount:1,max:1,req:{'advanced mycology':true}},
 			{type:'mult',value:1.75,req:{'advanced mycology':true}},
+			{type:'mult',value:5,req:{'myconument-building':true}},
 		],
 		req:{'mycology':true},
 		category:'production',
